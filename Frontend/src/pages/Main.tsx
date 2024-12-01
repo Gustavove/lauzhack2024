db08310da7d302924d79ef137e7cbc06bd73dade
Jordi <jordi.alejandre10@gmail.com>
2024-12-01 10:02:06 +0100

    styles: new styles for display and main

--- Frontend/src/pages/Main.tsx
+++ Frontend/src/pages/Main.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Displayer from "../components/Displayer";
 
-export const Main = ({ socket }) => {
+export const Main = ({ socket, gltfScene }) => {
   return (
     <>
-      <h1 className="p-5 flex justify-center items-center text-xl">
-        LauzHack 2024 - Logitech project
-      </h1>
+      <div className="h-screen bg-gradient-to-r from-gray-600 via-gray-700 to-gray-800 text-white w-screen">
+        <h1 className="p-5 flex justify-center items-center font-extrabold text-4xl">
+          BoardCast - Live Classroom
+        </h1>
 
-      <Displayer socket={socket} />
+        <Displayer gltfScene={gltfScene} socket={socket} />
+      </div>
     </>
   );
 };
